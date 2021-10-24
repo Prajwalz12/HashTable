d31de6066896c682351c0aec199139b309db5c22
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-24 18:14:08 +0530

    adding frquency program

--- HashTable_Day15/Program.cs
+++ HashTable_Day15/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace HashTable_Day15
+namespace HashTable
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Hash Table Programs");
+            Console.WriteLine("Welcome to Hash Table Program");
+            Console.WriteLine();
+
+
+            MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+            hash.Add("0", "to");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+
+            Console.WriteLine("Frequency of \'to\'  : " + hash.GetFrequencyOfWords("to"));
+            Console.WriteLine("Frequency of \'be\' : " + hash.GetFrequencyOfWords("be"));
+            Console.WriteLine("Frequency of \'or\' : " + hash.GetFrequencyOfWords("or"));
+            Console.WriteLine("Frequency of \'not\' : " + hash.GetFrequencyOfWords("not"));
         }
     }
 }
